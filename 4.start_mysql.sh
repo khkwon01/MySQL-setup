3ab56e6cd88842ffc38c16c04990b28519dc57ac
khkwon01 <khkwon01@gmail.com>
2023-01-05 18:07:55 +0900

    Update 4.start_mysql.sh

--- 4.start_mysql.sh
+++ 4.start_mysql.sh
@@ -2,5 +2,3 @@
 
 /mysql/mysql-latest/bin/mysqld --defaults-file=/mysql/etc/my.cnf --user=mysql &
 
-# tde 암호화 사용할때
-# /mysql/mysql-latest/bin/mysqld --defaults-file=/mysql/etc/my.cnf --early-plugin-load --user=mysql &
