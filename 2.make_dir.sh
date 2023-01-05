0f7eb09a2e0d0eab3eec5173443e1ba14adb29d7
khkwon01 <khkwon01@gmail.com>
2023-01-05 18:35:39 +0900

    Update 2.make_dir.sh

--- 2.make_dir.sh
+++ 2.make_dir.sh
@@ -9,8 +9,12 @@ mkdir -p /mysql/etc
 mkdir -p /mysql/mysql-latest
 mkdir -p /mysql/keyring
 
-
+# single mysql
 cp /root/tta-main/my.cnf_single /mysql/etc/my.cnf
+
+# cluster mysql (when cluster mysql install, remove the comment and then change server id according to assigned id)
+cp /root/tta-main/my.cnf_cluster /mysql/etc/my.cnf
+
 cp /root/tta-main/4.start_mysql.sh /mysql/start_mysql.sh
 cp /root/tta-main/4.stop_mysql.sh /mysql/stop_mysql.sh
 chmod 750 /mysql/start_mysql.sh
