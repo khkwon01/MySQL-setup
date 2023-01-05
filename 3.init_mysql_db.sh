1b464db5c1f11ef7f2d57bf105eed6d3b9d07b93
khkwon01 <khkwon01@gmail.com>
2023-01-05 15:43:40 +0900

    Update 3.init_mysql_db.sh

--- 3.init_mysql_db.sh
+++ 3.init_mysql_db.sh
@@ -2,5 +2,5 @@
 
 /mysql/mysql-latest/bin/mysqld --defaults-file=/mysql/etc/my.cnf --initialize --user=mysql
 
-echo "error check : " `grep -i ready /mysql/log/err_log.log`
-echo "root temp password : " `grep -i 'temporary password'`
+echo "error check : " `grep -i err /mysql/log/err_log.log`
+echo "root temp password : " `grep -i 'temporary password' /mysql/log/err_log.log | awk -F"host:" '{print $2}' `
