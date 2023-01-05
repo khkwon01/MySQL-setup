800e7829869f007e0e513a9fb58c2f9c58d2f071
khkwon01 <khkwon01@gmail.com>
2023-01-05 15:06:44 +0900

    Update 2.make_dir.sh

--- 2.make_dir.sh
+++ 2.make_dir.sh
@@ -8,5 +8,7 @@ mkdir -p /mysql/binlog
 mkdir -p /mysql/etc
 
 cp /root/tta-main/my.cnf_single /mysql/etc/my.cnf
+cp /root/tta-main/start_mysql.sh_single /mysql/start_mysql.sh
+chmod 750 /mysql/start_mysql.sh
 
 chown -R mysql:mysql /mysql
