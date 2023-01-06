261c44fd7e0f345f3d78d13b79a0a78a9a5ca42c
khkwon01 <khkwon01@gmail.com>
2023-01-06 11:28:28 +0900

    Update masking_test.sql

--- test/masking_test.sql
+++ test/masking_test.sql
@@ -1,7 +1,15 @@
 -- admin에서 masking function 생성
 CREATE FUNCTION gen_range RETURNS INTEGER SONAME 'data_masking.so';
 CREATE FUNCTION gen_rnd_email RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION gen_rnd_us_phone RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION mask_inner RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION mask_outer RETURNS STRING SONAME 'data_masking.so';
 
-
+-- 서비스 계정에서 
 use tde;
+select gen_range(1, 10);
+select gen_rnd_us_phone();
+select gen_rnd_email();
 
+select mask_inner(col1, 1, 1) from tb_enc ;
+select mask_outer(col1, 1, 1) from tb_enc ;
