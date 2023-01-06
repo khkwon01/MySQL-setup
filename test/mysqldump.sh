386133503acd31f21bd04b97d6df53557a1a9826
khkwon01 <khkwon01@gmail.com>
2023-01-06 11:37:03 +0900

    Update mysqldump.sh

--- test/mysqldump.sh
+++ test/mysqldump.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mysqldump -usvcuser -p -h127.0.0.1 --single-transaction --database tde > /mysql/tde.sql
+mysqldump -usvcuser -p -h127.0.0.1 --single-transaction --databases tde > /mysql/tde.sql
