6675aa4cf595c2773ac80aef847b9186473dbc44
khkwon01 <khkwon01@gmail.com>
2023-01-06 10:17:36 +0900

    Update json_test.sql

--- test/json_test.sql
+++ test/json_test.sql
@@ -1,11 +1,12 @@
 create database json;
 use json;
 
-create table json_table
-(
-    id bigint NOT NULL auto_increment,
-    doc json,
-    primary key(id)
+create table json_table ( id bigint NOT NULL AUTO_INCREMENT,
+    doc JSON,
+    primary key (id)
 );
 
 select * from json_table;
+
+insert into json_table(doc) value('{"A": "hello", "b": "test", "c": {"hello": 1}}');
+insert into json_table(doc) value('{"b": "hello"}'),('{"c":"c": {"hello": 1}}');
