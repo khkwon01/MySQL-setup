f7d0548ac88ba95158ee33f1671c87d859d757e3
khkwon01 <khkwon01@gmail.com>
2023-01-11 18:21:59 +0900

    Update masking_test.sql

--- test/masking_test.sql
+++ test/masking_test.sql
@@ -1,9 +1,19 @@
 -- admin에서 masking function 생성
+CREATE FUNCTION gen_blocklist RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION gen_dictionary RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION gen_dictionary_drop RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION gen_dictionary_load RETURNS STRING SONAME 'data_masking.so';
 CREATE FUNCTION gen_range RETURNS INTEGER SONAME 'data_masking.so';
 CREATE FUNCTION gen_rnd_email RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION gen_rnd_pan RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION gen_rnd_ssn RETURNS STRING SONAME 'data_masking.so';
 CREATE FUNCTION gen_rnd_us_phone RETURNS STRING SONAME 'data_masking.so';
 CREATE FUNCTION mask_inner RETURNS STRING SONAME 'data_masking.so';
 CREATE FUNCTION mask_outer RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION mask_pan RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION mask_pan_relaxed RETURNS STRING SONAME 'data_masking.so';
+CREATE FUNCTION mask_ssn RETURNS STRING SONAME 'data_masking.so';
+
 
 -- 서비스 계정에서 
 use tde;
