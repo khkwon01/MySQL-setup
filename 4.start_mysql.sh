22494e52e3d75d1af24b2fed817efdbd37ef6645
khkwon01 <khkwon01@gmail.com>
2023-01-12 13:52:20 +0900

    Update 4.start_mysql.sh

--- 4.start_mysql.sh
+++ 4.start_mysql.sh
@@ -1,4 +1,5 @@
 #/bin/bash
 
-/mysql/mysql-latest/bin/mysqld --defaults-file=/mysql/etc/my.cnf --user=mysql &
+#/mysql/mysql-latest/bin/mysqld --defaults-file=/mysql/etc/my.cnf --user=mysql &
+/mysql/mysql-latest/bin/mysqld_safe --defaults-file=/mysql/etc/my.cnf --user=mysql &
 
