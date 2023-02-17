0a13fe8aededacc5a3cc64d91db16767ffbb252b
khkwon01 <khkwon01@gmail.com>
2023-02-17 09:07:01 +0900

    Update 3.init_mysql_db.sh

--- 3.init_mysql_db.sh
+++ 3.init_mysql_db.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-chown -R mysql:mysql /mysql/mysql-latest
+chown -R mysql:mysql /home/mysql/mysql-latest
 
-/mysql/mysql-latest/bin/mysqld --defaults-file=/mysql/etc/my.cnf --initialize --user=mysql
+/home/mysql/mysql-latest/bin/mysqld --defaults-file=/home/mysql/etc/my.cnf --initialize --user=mysql
 
-echo "error check : " `grep -i err /mysql/log/err_log.log`
-echo "root temp password : " `grep -i 'temporary password' /mysql/log/err_log.log | awk -F"host:" '{print $2}' `
+echo "error check : " `grep -i err /home/mysql/log/err_log.log`
+echo "root temp password : " `grep -i 'temporary password' /home/mysql/log/err_log.log | awk -F"host:" '{print $2}' `
 
 # start mysql
-/mysql/start_mysql.sh > /dev/null
+/home/mysql/start_mysql.sh > /dev/null
 
 # sleep 5 until mysql server startup..
 sleep 5
 
 # password를 Ttatest1! 로 설정
-/mysql/mysql-latest/bin/mysql_secure_installation -h127.0.0.1
+/home/mysql/mysql-latest/bin/mysql_secure_installation -h127.0.0.1
