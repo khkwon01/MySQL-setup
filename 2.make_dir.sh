52150013c5c7c3c894959f728c2db31abf08d2a1
khkwon01 <khkwon01@gmail.com>
2023-02-17 09:06:12 +0900

    Update 2.make_dir.sh

--- 2.make_dir.sh
+++ 2.make_dir.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
 
-mkdir -p /mysql
-mkdir -p /mysql/data
-mkdir -p /mysql/temp
-mkdir -p /mysql/log
-mkdir -p /mysql/binlog
-mkdir -p /mysql/etc
-mkdir -p /mysql/mysql-latest
-mkdir -p /mysql/keyring
+mkdir -p /home/mysql
+mkdir -p /home/mysql/data
+mkdir -p /home/mysql/temp
+mkdir -p /home/mysql/log
+mkdir -p /home/mysql/binlog
+mkdir -p /home/mysql/etc
+mkdir -p /home/mysql/mysql-latest
+mkdir -p /home/mysql/keyring
 
 # single mysql
-cp /root/tta-main/my.cnf_single /mysql/etc/my.cnf
+cp /root/tta-main/my.cnf_single /home/mysql/etc/my.cnf
 
 # cluster mysql (when cluster mysql install, remove the comment and then change server id according to assigned id)
-# cp /root/tta-main/my.cnf_cluster /mysql/etc/my.cnf
+# cp /root/tta-main/my.cnf_cluster /home/mysql/etc/my.cnf
 
-cp /root/tta-main/4.start_mysql.sh /mysql/start_mysql.sh
-cp /root/tta-main/4.stop_mysql.sh /mysql/stop_mysql.sh
-chmod 750 /mysql/start_mysql.sh
-chmod 750 /mysql/stop_mysql.sh
+cp /root/tta-main/4.start_mysql.sh /home/mysql/start_mysql.sh
+cp /root/tta-main/4.stop_mysql.sh /home/mysql/stop_mysql.sh
+chmod 750 /home/mysql/start_mysql.sh
+chmod 750 /home/mysql/stop_mysql.sh
 
 chown -R mysql:mysql /mysql
-echo "export PATH=/mysql/mysql-latest/bin:\$PATH" >> /etc/profile
-echo "export PATH=/mysql/mysql-latest/bin:\$PATH" >> ~/.bashrc
+echo "export PATH=/home/mysql/mysql-latest/bin:\$PATH" >> /etc/profile
+echo "export PATH=/home/mysql/mysql-latest/bin:\$PATH" >> ~/.bashrc
 
 source /etc/profile
 . ~/.bashrc
