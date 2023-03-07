6aa5f83e265c98966db6a767c47cf90a381a2bcb
khkwon01 <khkwon01@gmail.com>
2023-03-07 11:54:28 +0900

    Update firewall_test.sql

--- test/firewall_test.sql
+++ test/firewall_test.sql
@@ -41,3 +41,33 @@ SELECT RULE FROM performance_schema.firewall_group_allowlist
 CALL mysql.sp_set_firewall_group_mode('fwgrp', 'PROTECTING');
 
 CALL mysql.sp_firewall_group_delist('fwgrp', 'fwtest@%');
+
+
+-- firewall 삭제
+DROP TABLE IF EXISTS mysql.firewall_group_allowlist;
+DROP TABLE IF EXISTS mysql.firewall_groups;
+DROP TABLE IF EXISTS mysql.firewall_membership;
+DROP TABLE IF EXISTS mysql.firewall_users;
+DROP TABLE IF EXISTS mysql.firewall_whitelist;
+UNINSTALL PLUGIN MYSQL_FIREWALL;
+UNINSTALL PLUGIN MYSQL_FIREWALL_USERS;
+UNINSTALL PLUGIN MYSQL_FIREWALL_WHITELIST;
+DROP FUNCTION IF EXISTS firewall_group_delist;
+DROP FUNCTION IF EXISTS firewall_group_enlist;
+DROP FUNCTION IF EXISTS mysql_firewall_flush_status;
+DROP FUNCTION IF EXISTS normalize_statement;
+DROP FUNCTION IF EXISTS read_firewall_group_allowlist;
+DROP FUNCTION IF EXISTS read_firewall_groups;
+DROP FUNCTION IF EXISTS read_firewall_users;
+DROP FUNCTION IF EXISTS read_firewall_whitelist;
+DROP FUNCTION IF EXISTS set_firewall_group_mode;
+DROP FUNCTION IF EXISTS set_firewall_mode;
+DROP PROCEDURE IF EXISTS mysql.sp_firewall_group_delist;
+DROP PROCEDURE IF EXISTS mysql.sp_firewall_group_enlist;
+DROP PROCEDURE IF EXISTS mysql.sp_reload_firewall_group_rules;
+DROP PROCEDURE IF EXISTS mysql.sp_reload_firewall_rules;
+DROP PROCEDURE IF EXISTS mysql.sp_set_firewall_group_mode;
+DROP PROCEDURE IF EXISTS mysql.sp_set_firewall_group_mode_and_user;
+DROP PROCEDURE IF EXISTS mysql.sp_set_firewall_mode;
+
+위에 object 삭제후 사용중이던 object에 release를 위해 restart 수행
