42f8ca87b4b6ddae6230fc8748e353a8c57f583c
khkwon01 <khkwon01@gmail.com>
2023-03-21 12:06:30 +0900

    Create legacy_code.cc

---
+++ api/c++/legacy_code.cc
@@ -0,0 +1,73 @@
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+#include <memory>
+#include <mysql/jdbc.h>
+
+#include <jdbc/mysql_connection.h>
+#include <jdbc/mysql_driver.h>
+#include <jdbc/cppconn/driver.h>
+#include <jdbc/cppconn/exception.h>
+#include <jdbc/cppconn/resultset.h>
+#include <jdbc/cppconn/statement.h>
+#include <jdbc/cppconn/prepared_statement.h>
+using namespace std;
+
+//for demonstration only. never save your password in the code!
+const string server = "tcp://127.0.0.1:3306";
+const string username = "root";
+const string password = "Ttatest1!";
+
+int main()
+{
+	sql::Driver *driver;
+	sql::Connection *con;
+	sql::Statement *stmt;
+	sql::PreparedStatement *pstmt;
+
+	try
+	{
+		driver = sql::mysql::get_driver_instance();
+		con = driver->connect(server, username, password);
+	}
+	catch (sql::SQLException e)
+	{
+		cout << "Could not connect to server. Error message: " << e.what() << endl;
+		system("pause");
+		exit(1);
+	}
+
+	//create database "tta4" ahead of time
+	con->createSchema("tta4")
+	con->setSchema("tta4");
+
+	stmt = con->createStatement();
+	stmt->execute("DROP TABLE IF EXISTS inventory");
+	cout << "Finished dropping table (if existed)" << endl;
+	stmt->execute("CREATE TABLE inventory (id serial PRIMARY KEY, name VARCHAR(50), quantity INTEGER);");
+	cout << "Finished creating table" << endl;
+	delete stmt;
+
+	pstmt = con->prepareStatement("INSERT INTO inventory(name, quantity) VALUES(?,?)");
+	pstmt->setString(1, "banana");
+	pstmt->setInt(2, 150);
+	pstmt->execute();
+	cout << "One row inserted." << endl;
+
+	pstmt->setString(1, "orange");
+	pstmt->setInt(2, 154);
+	pstmt->execute();
+	cout << "One row inserted." << endl;
+
+	pstmt->setString(1, "apple");
+	pstmt->setInt(2, 100);
+	pstmt->execute();
+	cout << "One row inserted." << endl;
+
+	delete pstmt;
+	delete con;
+	system("sleep 1");
+
+	return 0;
+}
